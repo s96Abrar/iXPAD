65e8ca46257cceb3d31852faa1eda6cad446afe9
AzadRashed <azadrashed3363@gmail.com>
2019-03-05 12:50:07 +0000

    Added action listener.

--- Source/src/iX/MainFrame.java
+++ Source/src/iX/MainFrame.java
@@ -2,11 +2,14 @@ package iX;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.FileDialog;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -21,10 +24,14 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame implements ActionListener{
-	private JButton btnOpen;
-	JButton btnSave;
-	JTextArea textEditor;
+	private Container frameContainer;
+	private JLabel appName;
+	private JPanel buttonPanel, textEditorPanel;
+	private JButton btnOpen, btnNewPage, btnSave, btnSaveAs, btnCopy, btnPaste, btnCut, btnUndo, btnRedo, btnSearch, btnBookmarkIt;
+	private JTextArea textEditor;
 	private Font f;
+	
+	private String workFilePath;
 
 	MainFrame() {
 		initComponents();
@@ -36,18 +43,18 @@ public class MainFrame extends JFrame implements ActionListener{
 		this.setTitle("iXPAD");
 		this.setLayout(null);
 		
-		Container frameContainer = this.getContentPane();
+		frameContainer = this.getContentPane();
 		frameContainer.setLayout(null);
 		f = new Font("Arial",Font.BOLD,18);
 				
 		// App name label
-		JLabel appName = new JLabel("iXPAD");
+		appName = new JLabel("iXPAD");
 		appName.setBounds(5,0,100,50);
 		
 		frameContainer.add(appName);
 		
 		// Control button panel
-		JPanel buttonPanel = new JPanel();
+		buttonPanel = new JPanel();
 		buttonPanel.setBounds(0,45,250,640);
 		buttonPanel.setBackground(Color.LIGHT_GRAY);
 		
@@ -60,16 +67,16 @@ public class MainFrame extends JFrame implements ActionListener{
 		
 		// All Control Buttons
 		btnOpen = new JButton("Open");
-		JButton btnNewPage = new JButton("New Page");
+		btnNewPage = new JButton("New Page");
 		btnSave = new JButton("Save");
-		JButton btnSaveAs = new JButton("Save As");
-		JButton btnCopy = new JButton("Copy");
-		JButton btnPaste = new JButton("Paste");
-		JButton btnCut = new JButton("Cut");
-		JButton btnUndo = new JButton("Undo");
-		JButton btnRedo = new JButton("Redo");
-		JButton btnSearch = new JButton("Search");
-		JButton btnBookmarkIt = new JButton("Bookmark It");
+		btnSaveAs = new JButton("Save As");
+		btnCopy = new JButton("Copy");
+		btnPaste = new JButton("Paste");
+		btnCut = new JButton("Cut");
+		btnUndo = new JButton("Undo");
+		btnRedo = new JButton("Redo");
+		btnSearch = new JButton("Search");
+		btnBookmarkIt = new JButton("Bookmark It");
 		
 		buttonPanel.add(btnOpen);
 		buttonPanel.add(btnNewPage);
@@ -83,7 +90,7 @@ public class MainFrame extends JFrame implements ActionListener{
 		buttonPanel.add(btnSearch);
 		buttonPanel.add(btnBookmarkIt);
 		
-		JPanel textEditorPanel = new JPanel();
+		textEditorPanel = new JPanel();
 		textEditorPanel.setLayout(null);
 		textEditorPanel.setBounds(260,0,1060,640);
 		textEditorPanel.setBackground(Color.lightGray);
@@ -118,16 +125,19 @@ public class MainFrame extends JFrame implements ActionListener{
 			
 			if (exec == JFileChooser.APPROVE_OPTION) {
 				File file = fileChooser.getSelectedFile();
-				String filePath = file.getPath();
+				String filePath = file.toString();
 				
 				try {
 					BufferedReader br = new BufferedReader(new FileReader(filePath));
 					String currentLine = "";
 					while ((currentLine = br.readLine()) != null) {
-						textEditor.setText(currentLine + "\n");
+						textEditor.append(currentLine + "\n");
 					}
 					
 					br.close();
+					
+					workFilePath = filePath;
+					setTitle(workFilePath + " - iXPAD");
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
@@ -135,18 +145,64 @@ public class MainFrame extends JFrame implements ActionListener{
 		}
 		
 		if (e.getSource() == btnSave) {
-			JFileChooser fileChooser = new JFileChooser();
-			fileChooser.showSaveDialog(this);
-			File file = fileChooser.getSelectedFile();
-			
-			try {
-				FileWriter fw = new FileWriter(file);
-				String text = textEditor.getText();
-				fw.write(text);
-				fw.close();
-			} catch (IOException ex) {
-				ex.printStackTrace();
+			String fileName = workFilePath;
+			File file = new File(fileName);
+			if (file.exists()) {
+				if (saveFile(fileName)) {
+					textEditor.requestFocus();
+				}
+			} else {
+				saveFileDialog();
 			}
-		}		
+		}
+		
+		if (e.getSource() == btnSaveAs) {
+			if (saveFileDialog()) {
+				textEditor.requestFocus();
+			}
+		}
+		
+		if (e.getSource() == btnCopy) {
+			textEditor.copy();
+		}
+		
+		if (e.getSource() == btnCut) {
+			textEditor.cut();
+		}
+		
+		if (e.getSource() == btnPaste) {
+			textEditor.paste();
+		}
+	}
+	
+	private boolean saveFileDialog() {
+		FileDialog fd = new FileDialog(new JFrame(), "Save File", FileDialog.SAVE);
+        fd.show();
+        if (fd.getFile() != null) {
+            String fileName = fd.getDirectory() + fd.getFile();
+
+            if (saveFile(fileName)) {
+                workFilePath = fileName;
+                setTitle(fileName.substring(fileName.lastIndexOf("\\") + 1) + "iXPAD");
+                
+                return true;
+            }
+        }
+        
+        return false;
+	}
+	
+	private boolean saveFile(String filePath) {
+		try {
+			DataOutputStream d = new DataOutputStream(new FileOutputStream(filePath));
+            String line = textEditor.getText();
+            d.writeBytes(line);
+            d.close();
+		} catch (Exception ex) {
+            System.out.println("File not found");
+			return false;
+		}
+		
+		return true;
 	}
 }
